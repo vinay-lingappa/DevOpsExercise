dd47677290b393f768ff4c3e769d7a33c2db60a6
vinay-lingappa <vinay.lingappa@paconsulting.com>
2017-05-24 19:25:18 +0530

    First commit
    First commit

---
+++ DevOpsExercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevOpsExercise.MvcApplication" Language="C#" %>
---
+++ DevOpsExercise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOpsExercise.Startup))]
+namespace DevOpsExercise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
